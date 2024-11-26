da58cf3d907d0f5f6d214a0187106e2bf3fa0cd6
jm4157 <jm4157@rit.edu>
2024-11-26 09:19:37 -0500

    Added image drawing

--- flashcard_gen.py
+++ flashcard_gen.py
@@ -9,6 +9,7 @@ from reportlab.lib.units import inch
 from reportlab.pdfbase.pdfmetrics import stringWidth, getFont, registerFont
 from reportlab.pdfbase.cidfonts import UnicodeCIDFont
 from reportlab.pdfbase.ttfonts import TTFont
+from PIL import Image
 
 
 MRGN_HORZ = int(0.75 * inch)
@@ -20,10 +21,12 @@ CARD_HGHT = int(4 * inch)
 # The program asumes that 2MRGN_HORZ + 2CARD_WIDTH + SPC_HORZ = 11 inches and
 # 2MRGN_VERT + 2CARD_HGHT + SPC_VERT = 8.5 inches to fit US letter paper
 
+SPC_TNY = 7
 SPC_SML = 14
 SPC_LG = 28
 HGHT_IMG = 72
 HGHT_TXT = 15
+SZ_QR = 28
 
 OFFSETS = [
     (MRGN_HORZ,                        MRGN_VERT), 
@@ -45,6 +48,9 @@ class CardInfo:
         self.xmpl_chn = xmpl_chn
         self.xmpl_eng = xmpl_eng
         self.qr       = qr
+
+        img_data = Image.open("pic/" + img)
+        self.img_ratio = round(img_data.width / img_data.height, 2)
 
 
 def draw_cardset(cards: list[CardInfo], canvas: Canvas):
@@ -70,20 +76,22 @@ def draw_card(data: CardInfo, canvas: Canvas, offset_x: int, offset_y: int):
     """
     canvas.rect(offset_x, offset_y, CARD_WDTH, CARD_HGHT, fill=False, stroke=True)
 
+    canvas.drawInlineImage("qr/" + data.qr, offset_x + CARD_WDTH - SPC_TNY - SZ_QR, offset_y + SPC_TNY, SZ_QR, SZ_QR)
+
     canvas.setFont('NotoSans', 11)
     x = offset_x + int(CARD_WDTH / 2) - int(stringWidth(data.xmpl_eng, 'NotoSans', 11) / 2)
-    y = offset_y + SPC_LG + HGHT_TXT
+    y = offset_y + 2* SPC_LG
     canvas.drawString(x, y, data.xmpl_eng)
 
     canvas.setFont('NotoSansSC', 11)
     x = offset_x + int(CARD_WDTH / 2) - int(stringWidth(data.xmpl_chn, 'NotoSansSC', 11) / 2)
-    y += 2 * HGHT_TXT + SPC_SML
+    y += HGHT_TXT + SPC_SML
     canvas.drawString(x, y, data.xmpl_chn)
 
     canvas.setFont('NotoSans', 14)
     ln = data.wrd_eng + " - " + data.phon
     x = offset_x + int(CARD_WDTH / 2) - int(stringWidth(ln, 'NotoSans', 14) / 2)
-    y += 2 * HGHT_TXT + SPC_LG
+    y += HGHT_TXT + SPC_LG
     canvas.drawString(x, y, ln)
 
     canvas.setFont('NotoSansSC', 14)
@@ -91,8 +99,11 @@ def draw_card(data: CardInfo, canvas: Canvas, offset_x: int, offset_y: int):
     y += HGHT_TXT + SPC_SML
     canvas.drawString(x, y, data.wrd_chn)
 
+    img_width = int(data.img_ratio * HGHT_IMG)
+    x = offset_x + int(CARD_WDTH / 2) - int(img_width / 2)
     y += HGHT_TXT + SPC_LG
-    canvas.drawString(offset_x, y, data.img)
+    canvas.drawInlineImage("pic/" + data.img, x, y, img_width, HGHT_IMG)
+
 
 
 if __name__ == "__main__":
@@ -101,10 +112,10 @@ if __name__ == "__main__":
     registerFont(TTFont('NotoSans', 'NotoSans-Regular.ttf'))
     registerFont(TTFont('NotoSansSC', 'NotoSansSC-Regular.ttf'))
 
-    cards = [CardInfo("Picture", "椅子", "Chair", "Yǐzǐ", "椅子很舒服。", "The chair is cozy.", "QR code"),
-             CardInfo("Picture", "桌子", "Table", "Zhuōzǐ", "我的盘子在桌子上。", "My plate is on the table.", "QR code"),
-             CardInfo("Picture", "床", "Bed", "Chuáng", "我喜欢晚上睡在我的床上。", "I like to sleep in my bed at night.", "QR code"),
-             CardInfo("Picture", "梳妆台", "Dresser", "Shūzhuāng tái", "梳妆台里面有衣服。", "The dresser has clothes inside.", "QR code")]
+    cards = [CardInfo("chair.jpg", "椅子", "Chair", "Yǐzǐ", "椅子很舒服。", "The chair is cozy.", "qr-default.png"),
+             CardInfo("table.png", "桌子", "Table", "Zhuōzǐ", "我的盘子在桌子上。", "My plate is on the table.", "qr-default.png"),
+             CardInfo("bed.jpg", "床", "Bed", "Chuáng", "我喜欢晚上睡在我的床上。", "I like to sleep in my bed at night.", "qr-default.png"),
+             CardInfo("dresser.jpg", "梳妆台", "Dresser", "Shūzhuāng tái", "梳妆台里面有衣服。", "The dresser has clothes inside.", "qr-default.png")]
 
     draw_cardset(cards, canvas)
 
